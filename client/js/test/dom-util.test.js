63be0c951b46f0c7a590213c8b1d2748cc53e7e1
Jerizon <JesseLowellReyes@gmail.com>
2017-04-26 09:59:06 -0700

    Test DOM creation functions

--- client/js/test/dom-util.test.js
+++ client/js/test/dom-util.test.js
@@ -1,23 +1,47 @@
-const { removeChildren,
-		createTH,
-		createTR,
-		createTD} = require('../dom-util');
+const {
+    removeChildren,
+    createTH,
+    createTR,
+    createTD
+} = require('../dom-util');
 
 describe('dom-util', () => {
-	it('removes one child', () => {
-		//set up intial state
-		const parent = document.createElement('DIV');
-		const child = document.createElement('STRONG');
-		parent.appendChild(child);
+    describe('removeChildren()', () => {
+        it('removes one child', () => {
+            //set up intial state
+            const parent = document.createElement('DIV');
+            const child = document.createElement('STRONG');
+            parent.appendChild(child);
 
-		//inspect inital state
-		expect(parent.childNodes.length).toBe(1);
-		expect(parent.childNodes[0]).toBe(child)
-		// execute code under test
-		removeChildren(parent);
+            //inspect inital state
+            expect(parent.childNodes.length).toBe(1);
+            expect(parent.childNodes[0]).toBe(child)
+                // execute code under test
+            removeChildren(parent);
 
-		// inspect resulting state
-		expect(parent.childNodes.length).toBe(0);
+            // inspect resulting state
+            expect(parent.childNodes.length).toBe(0);
+        });
+    });
 
-	})
-})
+    describe('DOM creation functions', () => {
+        describe('createTH', () => {
+            it('produces valid TH element', () => {
+                const el = createTH();
+                expect(el.tagName).toBe('TH');
+            })
+        });
+        describe('createTD', () => {
+            it('produces valid TD element', () => {
+                const el = createTD();
+                expect(el.tagName).toBe('TD');
+            })
+        });
+        describe('createTR', () => {
+            it('produces valid TR element', () => {
+                const el = createTR();
+                expect(el.tagName).toBe('TR');
+            })
+        });
+    });
+});
