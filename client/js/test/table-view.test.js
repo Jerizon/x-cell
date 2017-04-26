6042e4cd5313672c8288ee3fcc7968f16eb5c649
Jerizon <JesseLowellReyes@gmail.com>
2017-04-26 13:07:26 -0700

    Test rendering of data rows

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -3,27 +3,53 @@ const TableModel = require('../table-model');
 const TableView = require('../table-view');
 
 describe('table-view', () => {
-	beforeEach(() => {
-		//load HTML skeleton from disk and parse into DOM
-		const fixturePath = './client/js/test/fixtures/sheet-container.html';
-		const html = fs.readFileSync(fixturePath, 'utf8');
-		document.documentElement.innerHTML = html;
-	})
-	describe('table header', () => {
-		it('has valid column header lables', () => {
-			//set up the initial state 
-			const numCols = 6;
-			const numRows = 10;
-			const model = new TableModel(numCols, numRows);
-			const view = new TableView(model);
-			view.init();
+    beforeEach(() => {
+        //load HTML skeleton from disk and parse into DOM
+        const fixturePath = './client/js/test/fixtures/sheet-container.html';
+        const html = fs.readFileSync(fixturePath, 'utf8');
+        document.documentElement.innerHTML = html;
+    })
 
-			//inspect the inital state
-			let ths = document.querySelectorAll('THEAD TH');
-			expect(ths.length).toBe(numCols);
-			
-			let labelTexts = Array.from(ths).map(el => el.textContent);
-			expect(labelTexts).toEqual(['A', 'B', 'C', 'D', 'E', 'F']);
-		});
-	});
-}); 
+    describe('table body', () => {
+        it('has the size', () => {
+            // set up the inital state
+            const numCols = 6;
+            const numRows = 10;
+            const model = new TableModel(numCols, numRows);
+            const view = new TableView(model);
+            view.init();
+
+            // inspect the inital state
+            let ths = document.querySelectorAll('THEAD TH');
+            expect(ths.length).toBe(numCols);
+        });
+        it('fills in values from the model', () => {
+        	//set up the inital state
+        	const model = new TableModel(3, 3);
+        	const view = new TableView(model);
+        	model.setValue({col: 2, row: 1}, '123');
+        	view.init();
+        	//inspect the inital state
+        	const trs = document.querySelectorAll('TBODY TR');
+        	expect(trs[1].cells[2].textContent).toBe('123');
+        });
+    });
+
+    describe('table header', () => {
+        it('has valid column header lables', () => {
+            //set up the initial state 
+            const numCols = 6;
+            const numRows = 10;
+            const model = new TableModel(numCols, numRows);
+            const view = new TableView(model);
+            view.init();
+
+            //inspect the inital state
+            let ths = document.querySelectorAll('THEAD TH');
+            expect(ths.length).toBe(numCols);
+
+            let labelTexts = Array.from(ths).map(el => el.textContent);
+            expect(labelTexts).toEqual(['A', 'B', 'C', 'D', 'E', 'F']);
+        });
+    });
+});
