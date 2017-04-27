fa21b6282093c4c6ca34285ca503a6751005fde4
Jerizon <JesseLowellReyes@gmail.com>
2017-04-27 13:16:54 -0700

    Column headers now go to AA, AB, etc, after Z

--- client/js/array-util.js
+++ client/js/array-util.js
@@ -5,8 +5,19 @@ const getRange = function(fromNum, toNum) {
 const getLetterRange = function(firstLetter='A', numLetters) {
 	const rangeStart = firstLetter.charCodeAt(0);
 	const rangeEnd = rangeStart + numLetters-1;
-	return getRange(rangeStart, rangeEnd)
-		.map(charCode => String.fromCharCode(charCode));
+	let extendedLetterRange = [];
+	for(let letter = 0; letter < numLetters; letter ++) {
+		const aToZRange = Math.floor(letter/26);
+		if(aToZRange < 1 ){
+			extendedLetterRange.push(String.fromCharCode(letter + 65));
+		} else {
+			extendedLetterRange.push(String.fromCharCode(aToZRange + 64) + String.fromCharCode((letter % 26)+ 65));
+	}
+		}
+		
+	/*return getRange(rangeStart, rangeEnd)
+		.map(charCode => String.fromCharCode(charCode));*/
+		return extendedLetterRange;
 }
 module.exports = {
 	getRange: getRange,
