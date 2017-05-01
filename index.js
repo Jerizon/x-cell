e6bd06f4e225ded085049cc906d41ec6ea1f640f
Jerizon <JesseLowellReyes@gmail.com>
2017-04-30 20:32:35 -0700

    Update index for browserify

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
 server.listen(app.get('port'), () => {
 	console.log(`Server listening on port ${app.get('port')}...`);
-})
+});
